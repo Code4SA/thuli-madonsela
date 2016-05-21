4c55595e7dbbcb98a5dbecfc1c75da56bfb3f682
lailahr <lailyseven@gmail.com>
2016-05-21 15:04:54 +0200

    create embed.js

---
+++ embed.js
@@ -0,0 +1,8 @@
+if (document.location.hostname == "localhost") {
+    var baseurl = "";
+} else {
+    var baseurl = "https://code4sa.org/thuli-madonsela/";
+}
+document.write('<div id="code4sa-thuli-madonsela-index"></div>');
+document.write('<script type="text/javascript" src="' + baseurl + 'js/pym.js"></script>');
+document.write("<script>var pymParent = new pym.Parent('code4sa-thuli-madonsela-index', '" + baseurl + "index.html', {});</script>");
